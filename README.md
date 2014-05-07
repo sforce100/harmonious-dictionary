ec8e2a5e4d2abb4a2c2033c8e0473f44f3d48757
hzh <sforce1000@gmail.com>
2014-05-07 21:57:44 +0800

    －修复最后一位match的时候不能匹配 －增加readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 harmonious-dictionary
 =====================
+
+这是一个nodejs的中英文过滤词库
+
+=====================
+
+### 使用方法
+
+```
+var Harmonious = require('../harmonious');
+
+var harmonious = new Harmonious({cnPath: "./chinese_dictionary.yml", enPath: "./harmonious_english.yml"});
+
+harmonious.clean('xxoo == fuck'); //输出 xxoo == ****
+```
+
+test里面有词库，不介意可以拿去用。
--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
 var Harmonious = require('../harmonious');
 
-var harmonious = new Harmonious({cnPath: "/Users/huangzihao/mytest/nodejs/chinese_dictionary.txt", enPath: "/Users/huangzihao/mytest/nodejs/harmonious_english.yml"});
+var harmonious = new Harmonious({cnPath: "./chinese_dictionary.yml", enPath: "./harmonious_english.yml"});
 
 console.info(harmonious.clean('我是毛泽东是豆比'));
 console.info(harmonious.clean('xxoo毛泽东是豆比'));
 console.info(harmonious.clean('豆比 fuck 毛爷爷'));
+console.info(harmonious.clean('豆比fuck毛爷爷'));
+console.info(harmonious.clean('豆比的法轮功haha'));
+console.info(harmonious.clean('豆比的18禁'));
+console.info(harmonious.clean('脱光1234567'));
+console.info(harmonious.clean('xxoo == fuck'));
+console.info(harmonious.clean('xxoo==fuck'));
